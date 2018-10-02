84b326203d7895ed828af13a179478274b4ed53f
Tommy H. Yeargin, Jr <yeargin2018@gmail.com>
2018-10-01 22:40:15 -0400

    commit

--- Archives/body2.js
+++ Archives/body2.js
@@ -1,4 +1,4 @@
-$("#menu2").load("menu2.txt");
+$("#menu2").load("../PageElements/menu2.txt");
 $("#201810010000").load("../PageElements/201810010000.txt");
 /*$("#Welcome").load("PageElements/Welcome.txt");*/
 // $("#menu").load("menu.txt");
